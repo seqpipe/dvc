3633dd9157c6c5a177b475b829c9bcaec8677995
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-04-24 12:22:45 +0300

    reduce the default number of jobs to run simultaneously...

--- dvc/cli.py
+++ dvc/cli.py
@@ -137,7 +137,7 @@ def parse_args(argv=None):
     parent_cache_parser.add_argument('-j',
                         '--jobs',
                         type=int,
-                        default=8*cpu_count(),
+                        default=2,
                         help='Number of jobs to run simultaneously.')
     parent_cache_parser.add_argument(
                         'targets',
