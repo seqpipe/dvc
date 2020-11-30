163f5d7618d4c7a3eed399112d92b6bb416394fe
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2020-11-30 18:21:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
     "google-compute-engine==2.4.1", #required by boto
     "google-cloud-storage==1.11",
     "google-cloud==0.24.0",
-    "google-cloud-core==0.28",
+    "google-cloud-core==0.24",
     "google-api-core==0.1.1",
     "configparser==3.5.0",
     "zc.lockfile>=1.2.1",
