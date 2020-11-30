fdb13a31eb8141ce76b3ccb36e744573f131ce59
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2020-11-30 18:02:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ install_requires = [
     "boto3==1.7.4",
     "ply==3.8", #required by google-gax
     "google-compute-engine==2.4.1", #required by boto
-    "google-cloud-storage==1.14.0",
+    "google-cloud-storage==1.11",
     "configparser==3.5.0",
     "zc.lockfile>=1.2.1",
     "future>=0.16.0",
     "google-cloud==0.24.0",
+    "google-cloud-core==0.24",
     "colorama>=0.3.9",
     "configobj>=5.0.6",
     "networkx>=1.11",
